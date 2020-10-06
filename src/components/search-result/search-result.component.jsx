ae570157c58a4c8f650bbdea6b07f23a44e345c8
mrrezaahmadi <mrrezaahmadi>
2020-10-06 21:30:18 +0330

    fetching data from google custom search engine and pass the attribtes as props to SearchResult component

--- src/components/search-result/search-result.component.jsx
+++ src/components/search-result/search-result.component.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
+import React from "react";
 
 // Styles
-import './search-result.styles.scss'
+import "./search-result.styles.scss";
 
-const SearchResult = () => {
-    return (
-        <div className="search-result">
-            <div className="header">
-                <a href="#" target="_blank">header</a>
-            </div>
-            <div className="subheader">
-                subheader
-            </div>
-            <div className="content">
-                <div className="thumbnail">content thumbnail</div>
-                <div className="text">content text</div>
-            </div>
-        </div>
-    )
-}
+const SearchResult = ({ link, title, subtitle, textContent }) => {
+	return (
+		<div className="search-result">
+			<div className="header">
+				<a href={link} target="_blank">
+					{title}
+				</a>
+			</div>
+			<div className="subheader">{subtitle}</div>
+			<div className="content">
+				<div className="thumbnail">content thumbnail</div>
+				<div className="text">{textContent}</div>
+			</div>
+		</div>
+	);
+};
 
-export default SearchResult
+export default SearchResult;
--- src/pages/home-page/home-page.component.jsx
+++ src/pages/home-page/home-page.component.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
 
 // Styles
 import "./home-page.styles.scss";
@@ -9,15 +10,32 @@ import SearchResult from "../../components/search-result/search-result.component
 import CustomButton from "../../components/custom-btn/custom-btn.component";
 import Skeleton from "../../components/search-result-skeleton/search-result-skeleton.component";
 
-const HomePage = () => {
+// Redux actions
+import { fetchSearchStartAsync } from "../../redux/results/results.thunk";
+
+const HomePage = ({ isFetching, results, fetchSearchStartAsync }) => {
+	const [searchValue, setSearchValue] = useState("apple");
+	const [pageIndex, setPageIndex] = useState(0);
+
+	useEffect(() => {
+		fetchSearchStartAsync(searchValue, pageIndex);
+	}, [searchValue, pageIndex]);
+
 	return (
 		<div className="home-page">
 			<SearchForm />
 			<div className="search-results">
-				{[...Array(10)].map((_, i) => (
-					<SearchResult key={i} />
-					// <Skeleton key={i} />
-				))}
+				{isFetching
+					? [...Array(10)].map((_, i) => <Skeleton key={i} />)
+					: results.map((result, i) => (
+							<SearchResult
+								key={i}
+								link={result.link}
+								title={result.title}
+								subtitle={result.link}
+								textContent={result.snippet}
+							/>
+					  ))}
 			</div>
 			<div className="navigation-btns">
 				<CustomButton buttonText={"Previous Page"} />
@@ -27,4 +45,9 @@ const HomePage = () => {
 	);
 };
 
-export default HomePage;
+const mapStateToProps = (state) => ({
+	results: state.results.resultItems,
+	isFetching: state.results.isFetching,
+});
+
+export default connect(mapStateToProps, { fetchSearchStartAsync })(HomePage);
